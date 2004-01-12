76802196f330becdae43b31f122097435aa29249
Ken Williams <kwilliams@cpan.org>
2004-01-12 14:13:43 +0000

    Version bump

--- lib/ExtUtils/CBuilder.pm
+++ lib/ExtUtils/CBuilder.pm
@@ -5,7 +5,7 @@ use File::Path ();
 use File::Basename ();
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.00_03';
+$VERSION = '0.01';
 
 # Okay, this is the brute-force method of finding out what kind of
 # platform we're on.  I don't know of a systematic way.  These values
