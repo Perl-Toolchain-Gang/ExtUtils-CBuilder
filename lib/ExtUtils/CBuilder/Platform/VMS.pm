45dfbf8a80db2b42d955f88f88227ff068a09a78
Ken Williams <kwilliams@cpan.org>
2005-09-13 02:38:21 +0000

    Latest patch from John Malmberg for VMS goodness

--- lib/ExtUtils/CBuilder/Platform/VMS.pm
+++ lib/ExtUtils/CBuilder/Platform/VMS.pm
@@ -7,13 +7,13 @@ use vars qw($VERSION @ISA);
 $VERSION = '0.12';
 @ISA = qw(ExtUtils::CBuilder::Base);
 
-sub need_prelink { 1 }
+sub need_prelink { 0 }
 
 sub arg_include_dirs {
   my ($self, @dirs) = @_;
 
-  # VMS can only have one include list, remove the one from config.
-  if ($self->{config}{ccflags} =~ s{/include=\(([^\)]*)\)} {}i) {
+  # VMS can only have one include list, add the one from config.
+  if ($self->{config}{ccflags} =~ s{/inc[^=]+(?:=)+(?:\()?([^\/\)]*)} {}i) {
     unshift @dirs, $1;
   }
   return unless @dirs;
@@ -28,16 +28,17 @@ sub _do_link {
   $objects = [$objects] unless ref $objects;
   
   # VMS has two option files, the external symbol, and to pull in PerlShr
-  $objects->[-1] .= ',';
-  push @$objects, 'sys$disk:[]' . @temp_files[0] . '/opt,';
-  push @$objects, $self->perl_inc() . 'PerlShr.Opt/opt';
+  if ($args{lddl}) {
+    my @temp_files =
+      $self->prelink(%args, dl_name => $args{module_name});
 
-  # Need to create with the same name as DynaLoader will load with.
-  if (defined &DynaLoader::mod2fname) {
-    my $out = $args{$type} || $self->$type($objects->[0]);
-    my ($dev,$dir,$file) = File::Spec->splitpath($out);
-    $file = DynaLoader::mod2fname([$file]);
-    $args{$type} = File::Spec->catpath($dev,$dir,$file);
+    $objects->[-1] .= ',';
+
+    # If creating a loadable library, the link option file is needed.
+    push @$objects, 'sys$disk:[]' . $temp_files[0] . '/opt,';
+
+    # VMS always needs the option file for the Perl shared image.
+    push @$objects, $self->perl_inc() . 'PerlShr.Opt/opt';
   }
 
   return $self->SUPER::_do_link($type, %args, objects => $objects);
@@ -58,6 +59,22 @@ sub arg_exec_file {
 sub arg_share_object_file {
   my ($self, $file) = @_;
   return ("$self->{config}{lddlflags}=$file");
+}
+
+
+sub lib_file {
+  my ($self, $dl_file) = @_;
+  $dl_file =~ s/\.[^.]+$//;
+  $dl_file =~ tr/"//d;
+  $dl_file = $dl_file .= '.' . $self->{config}{dlext};
+
+  # Need to create with the same name as DynaLoader will load with.
+  if (defined &DynaLoader::mod2fname) {
+    my ($dev,$dir,$file) = File::Spec->splitpath($dl_file);
+    $file = DynaLoader::mod2fname([$file]);
+    $dl_file = File::Spec->catpath($dev,$dir,$file);
+  }
+  return $dl_file;
 }
 
 1;
