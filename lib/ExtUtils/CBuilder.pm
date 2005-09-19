1b82673164e0965826809615218642f92a80fb4a
Ken Williams <kwilliams@cpan.org>
2005-09-19 19:08:25 +0000

    Version bump

--- lib/ExtUtils/CBuilder.pm
+++ lib/ExtUtils/CBuilder.pm
@@ -5,7 +5,7 @@ use File::Path ();
 use File::Basename ();
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.13_01';
+$VERSION = '0.14';
 $VERSION = eval $VERSION;
 
 # Okay, this is the brute-force method of finding out what kind of
