2f6f903bd1223b31aa6b04e74112d864706b34a7
Ken Williams <kwilliams@cpan.org>
2005-09-26 01:36:27 +0000

    Oops, hand-misapplied Ilya's patch

--- lib/ExtUtils/CBuilder/Base.pm
+++ lib/ExtUtils/CBuilder/Base.pm
@@ -198,7 +198,6 @@ sub _do_link {
 		      $self->extra_link_args_after_prelink(%args, dl_name => $args{module_name},
 							   prelink_res => \@temp_files));
 
-  my @linker_flags = $self->split_like_shell($args{extra_linker_flags});
   my @output = $args{lddl} ? $self->arg_share_object_file($out) : $self->arg_exec_file($out);
   my @shrp = $self->split_like_shell($cf->{shrpenv});
   my @ld = $self->split_like_shell($cf->{ld});
