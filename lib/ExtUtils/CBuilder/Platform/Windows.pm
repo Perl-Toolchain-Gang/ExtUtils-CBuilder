8b4c6e375e591a122365eb3dbe4f2d063dabc656
David Golden <dagolden@cpan.org>
2009-06-28 03:23:59 +0000

    Changes, lib/***.pm - bump version

--- lib/ExtUtils/CBuilder/Platform/Windows.pm
+++ lib/ExtUtils/CBuilder/Platform/Windows.pm
@@ -10,7 +10,7 @@ use ExtUtils::CBuilder::Base;
 use IO::File;
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.25_01';
+$VERSION = '0.25_02';
 @ISA = qw(ExtUtils::CBuilder::Base);
 
 sub new {
--- lib/ExtUtils/CBuilder/Platform/os2.pm
+++ lib/ExtUtils/CBuilder/Platform/os2.pm
@@ -4,7 +4,7 @@ use strict;
 use ExtUtils::CBuilder::Platform::Unix;
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.25_01';
+$VERSION = '0.25_02';
 @ISA = qw(ExtUtils::CBuilder::Platform::Unix);
 
 sub need_prelink { 1 }
