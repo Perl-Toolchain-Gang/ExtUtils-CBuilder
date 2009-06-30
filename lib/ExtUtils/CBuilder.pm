efdd9404a5fb1edfdb1507d89b2d2d7c5a8ec2f1
David Golden <dagolden@cpan.org>
2009-06-30 00:15:38 +0000

    Changes, lib/***.pm - bump version

--- lib/ExtUtils/CBuilder.pm
+++ lib/ExtUtils/CBuilder.pm
@@ -5,7 +5,7 @@ use File::Path ();
 use File::Basename ();
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.26';
+$VERSION = '0.26_01';
 $VERSION = eval $VERSION;
 
 # Okay, this is the brute-force method of finding out what kind of
--- lib/ExtUtils/CBuilder/Base.pm
+++ lib/ExtUtils/CBuilder/Base.pm
@@ -9,7 +9,7 @@ use Text::ParseWords;
 use IO::File;
 
 use vars qw($VERSION);
-$VERSION = '0.26';
+$VERSION = '0.26_01';
 
 sub new {
   my $class = shift;
