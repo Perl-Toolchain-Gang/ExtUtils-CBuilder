4acfe3c739c16ce16ec2b3a32f883bab78be2692
David Golden <dagolden@cpan.org>
2009-07-04 14:58:04 +0000

    Updating version/Changes for release

--- lib/ExtUtils/CBuilder.pm
+++ lib/ExtUtils/CBuilder.pm
@@ -5,7 +5,7 @@ use File::Path ();
 use File::Basename ();
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.26_02';
+$VERSION = '0.2602';
 $VERSION = eval $VERSION;
 
 # Okay, this is the brute-force method of finding out what kind of
--- lib/ExtUtils/CBuilder/Base.pm
+++ lib/ExtUtils/CBuilder/Base.pm
@@ -9,7 +9,7 @@ use Text::ParseWords;
 use IO::File;
 
 use vars qw($VERSION);
-$VERSION = '0.26_02';
+$VERSION = '0.2602';
 
 sub new {
   my $class = shift;
