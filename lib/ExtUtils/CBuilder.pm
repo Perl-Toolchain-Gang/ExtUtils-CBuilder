32203fc464268832719b3d59e973c8a89702040e
David Golden <dagolden@cpan.org>
2009-10-29 21:20:19 -0400

    Bump version to 0.27 for production release

--- lib/ExtUtils/CBuilder.pm
+++ lib/ExtUtils/CBuilder.pm
@@ -5,7 +5,7 @@ use File::Path ();
 use File::Basename ();
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.26_05';
+$VERSION = '0.27';
 $VERSION = eval $VERSION;
 
 # Okay, this is the brute-force method of finding out what kind of
