e9c3d20887f40ba6a93b6688c519b63d2cb0ce28
David Golden <dagolden@cpan.org>
2011-01-21 15:10:30 -0500

    Update developer tools and docs

--- devtools/bleadcheck.pl
+++ devtools/bleadcheck.pl
@@ -1,23 +1,38 @@
 #!/usr/bin/perl
 
-# A script to check a local copy against bleadperl, generating a blead
+# A script to check a local copy against bleadperl, generating a
 # patch if they're out of sync.  The path to bleadperl is required.
 # An optional directory argument will be chdir()-ed into before comparing.
 
+# NOTE -- now that EU::CB has blead as upstream, the generated diff
+# is to be used to bring the local copy into sync with blead
+
 use strict;
+use warnings;
+use File::Spec::Functions;
+
 my $blead = shift @ARGV
   or die "Usage: $0 <bleadperl-src> [ExtUtils-CBuilder-src]\n";
 
 chdir shift() if @ARGV;
 
+die "Doesn't look like $blead contains the perl source"
+  unless -f catfile($blead, 'perl.c');
 
-diff( "$blead/lib/ExtUtils/CBuilder.pm", "lib/ExtUtils/CBuilder.pm");
+my $HEAD = catfile($blead, qw/.git HEAD/);
+my $head = do { local (@ARGV, $/) = $HEAD; <> };
+my ($ref) = $head =~ m{ref: (.*)};
 
-diff( "$blead/lib/ExtUtils/CBuilder", "lib/ExtUtils/CBuilder",
-      qw(t Changes .svn) );
+my $commit = do { local (@ARGV, $/) = catfile($blead, '.git', $ref); <> };
 
-diff( "$blead/lib/ExtUtils/CBuilder/t", "t",
-      qw(.svn) );
+$commit = substr($commit, 0, 8);
+
+print STDERR "Comparing with $commit\n";
+
+my $upstream = catdir($blead, qw/dist ExtUtils-CBuilder/);
+
+diff( "lib", "$upstream/lib" );
+diff( "t", "$upstream/t" );
 
 ######################
 sub diff {
